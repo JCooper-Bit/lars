5a89bc0d0b24b045293f98df53831a5795e9f844
JCooper-Bit <JCooper-Bit@users.noreply.github.com>
2025-10-26 16:40:50 +0000

    deploy: 3edb5a82e82d98aae7b30f8dc10ec2e00bd53b07

--- src-files.js
+++ src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["lars",["",[["vector",[],["mod.rs","scalar.rs","vector2.rs","vector3.rs"]]],["lib.rs"]]]]');
-//{"start":19,"fragment_lengths":[89]}
+createSrcSidebar('[["lars",["",[["matrix",[],["mat2.rs","mod.rs"]],["vector",[],["mod.rs","scalar.rs","vector2.rs","vector3.rs"]]],["lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[124]}
