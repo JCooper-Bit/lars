fe5ef6b4a62753067fde9a18ca67a87f5c1a1511
JCooper-Bit <JCooper-Bit@users.noreply.github.com>
2025-10-26 15:58:31 +0000

    deploy: a7a52ed32e6b169bfaf2e2b50c747f332b80953f

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lars",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"lars/struct.Vec2.html\" title=\"struct lars::Vec2\">Vec2</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"lars/struct.Vec3.html\" title=\"struct lars::Vec3\">Vec3</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[484]}
